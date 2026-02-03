0b5910fa2f7378e7258f7cf687fb5792570420a3
Jershw43 <95701884+jershw43@users.noreply.github.com>
2026-02-02 22:32:24 -0500

    Working calculator, missing comments

---
+++ Assignment1/Average.java
@@ -0,0 +1,70 @@
+/*
+	Program name: Assignment 1, Averaging calculator
+	Course: CMSC 3320, Technical Computing Using Java
+	Group: #3
+	Members:
+		Shawn Gallagher - GAL82896@pennwest.edu
+		Lucas Giovannelli - GIO07221@pennwest.edu
+		Joshua Watson - WAT93888@pennwest.edu
+		
+	
+*/
+import java.io.*;
+
+class Average
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+
+        double val = 0;
+        double sum = 0;
+        double avg = 0;
+        int count = 0;
+
+        while (val >=0 && val <=100 ||count == 0)
+        {
+            System.out.print("Enter a real number: ");
+            System.out.flush();
+            line = stdin.readLine();
+
+            boolean valid = true;
+
+            try
+            {
+                val = Double.valueOf(line).doubleValue();
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.println("Invalid data entered. Please enter a real number.");
+                valid = false;
+            }
+
+            if (valid)
+            {
+                if (!Double.isNaN(val))
+                {
+                    if (val >= 0 && val <= 100)
+                    {
+                        sum += val;
+                        count++;
+
+                        
+                    }
+                }
+                else
+                {
+                    System.out.println("NaN entered. Please enter a valid number.");
+                }
+            }
+        }
+		if (count > 0)
+		{
+			avg = sum / count;
+			System.out.println("Average: " + avg);
+			System.out.println("Sum: " + sum);
+			System.out.println("Number of grades entered: " + count);
+		}
+    }
+}
