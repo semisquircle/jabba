a25ba509946796adf24e9af7daa396c8f2d71d87
semisquircle <semisquircle@gmail.com>
2026-02-02 23:22:49 -0500

    done with assignment 1

--- Assignment1/Average.java
+++ Assignment1/Average.java
@@ -1,47 +1,54 @@
 /*
-	Program name: Assignment 1, Averaging calculator
+	Program name: Assignment 1, Average Program
 	Course: CMSC 3320, Technical Computing Using Java
 	Group: #3
 	Members:
 		Shawn Gallagher - GAL82896@pennwest.edu
 		Lucas Giovannelli - GIO07221@pennwest.edu
 		Joshua Watson - WAT93888@pennwest.edu
-		
-	
 */
 import java.io.*;
-/* creates a class names Average */
+
 class Average
 {
     public static void main(String[] args) throws IOException
     {
         BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
         String line;
-/* initializes variables */
+
+        // Initializes variables
         double val = 0;
         double sum = 0;
         double avg = 0;
         int count = 0;
-/* prompts user for real number input between 0 and 100 */
-        while (val >=0 && val <=100 ||count == 0)
+
+        System.out.println();
+        System.out.println("––––––––––––––––––––––––––––––––––––––––––––––––––");
+        System.out.println("GRADE AVERAGER");
+        System.out.println("––––––––––––––––––––––––––––––––––––––––––––––––––");
+
+        // Prompts user for real number input between 0 and 100
+        while (val >= 0 && val <= 100)
         {
-            System.out.print("Enter a real number: ");
+            System.out.print("Enter a grade (0-100): ");
             System.out.flush();
             line = stdin.readLine();
 
             boolean valid = true;
-/* tries to turn input into double */ 
+
+            // Tries to turn input into double
             try
             {
                 val = Double.valueOf(line).doubleValue();
             }
-/* catches bad inputs */
+            // Catches bad inputs
             catch (NumberFormatException e)
             {
                 System.out.println("Invalid data entered. Please enter a real number.");
                 valid = false;
             }
-/* checks input, adds to sum, counts # of inputs*/ 
+
+            // Checks input, adds to sum, counts # of inputs
             if (valid)
             {
                 if (!Double.isNaN(val))
@@ -50,8 +57,6 @@ class Average
                     {
                         sum += val;
                         count++;
-
-                        
                     }
                 }
                 else
@@ -60,13 +65,17 @@ class Average
                 }
             }
         }
-/* finds and prints average, sum, and input count*/
+
+        // Calculates and prints average, sum, and input count
 		if (count > 0)
 		{
 			avg = sum / count;
+            System.out.println("––––––––––––––––––––––––––––––––––––––––––––––––––");
 			System.out.println("Average: " + avg);
 			System.out.println("Sum: " + sum);
 			System.out.println("Number of grades entered: " + count);
+            System.out.println("––––––––––––––––––––––––––––––––––––––––––––––––––");
+            System.out.println();
 		}
     }
 }
